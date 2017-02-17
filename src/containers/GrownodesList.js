613e4cad60b26e6c20b35e25a1d51cc41c37d938
Kevin <keyeh@calpoly.edu>
2017-02-16 20:23:08 -0800

    send fcm from grownodes list

--- src/containers/GrownodesList.js
+++ src/containers/GrownodesList.js
@@ -3,7 +3,7 @@ import Base from '../utils/base'
 import { connect } from 'react-redux'
 import { sendToEveryone, addTodoEveryone } from '../actions/notifications'
 import { fetchUsers } from '../actions/users'
-
+import request from 'request'
 class GrownodesList extends Component {
   constructor(props) {
     super(props);
@@ -33,6 +33,41 @@ class GrownodesList extends Component {
         created_at: new Date()
       }
     })
+
+
+
+
+
+
+    Base.fetch(`fcm_tokens/`, {
+      context: this,
+      queries: {
+        orderByChild: 'uid',
+        equalTo: this.state.grownodes[grownodeId].owner_uid
+      }
+    }).then(data => {
+      const fcm_tokens = Object.keys(data)
+
+      var options = {
+        url: 'https://fcm.googleapis.com/fcm/send',
+        method: 'POST',
+        headers: {
+          'Authorization': 'key=AAAAetYor2s:APA91bHkaQM2PAmL8rODHC4ChVSFpADsi9JXJoVzAVb4FnuMnio55OONzSfEBffVIXuB3otnKE88O3MQp6kSURnoUIY2SBmw0SHI9GXsADE-vt2T3s1e4Lz4QFjJccfo2W1ZF1nu00h_csyr70b6_kzOm38DBPlqqg',
+        },
+        json:true,
+        body: {
+          registration_ids: fcm_tokens,
+          data: {
+            title,
+            body
+          }
+        }
+      }
+
+      request(options, function (error, response, body) {
+        console.log(body)
+      })
+    })
   }
 
   renderListItem() {
@@ -41,7 +76,7 @@ class GrownodesList extends Component {
       return (
         <div className='card' style={{marginBottom: 50}} key={key}>
           <div className='card-header'>{key}</div>
-          <form className='card-block' onSubmit={this.addTodo}>
+          <form className='card-block' onSubmit={this.addTodo.bind(this)}>
             <input type='hidden' name='grownodeId' value={key} />
             <div className='input-group'>
               <input type='text' className='form-control' placeholder='title' name='title' required />
